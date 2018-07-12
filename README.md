51eb5cc010e64c5f26cefc2d4305dba86b2dfc17
William Cottam <wecottam@yahoo.com>
2018-07-11 20:00:23 -0600

    added dropdown button

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Basic button code from Reactstrap with Bootstrap css imported: https://reactstra
 
 Colored buttons, Outlined colored buttons, button sizes including block level (added medium-sized buttons),
 Active/Disabled buttons, checkbox and radio buttons.
+
+Dropdown buttons: https://reactstrap.github.io/components/button-dropdown/
--- src/Buttons.js
+++ src/Buttons.js
@@ -5,6 +5,7 @@ import ReactstrapOutlineButtons from './ReactstrapOutlineButtons';
 import ReactstrapButtonSizes from './ReactstrapButtonSizes';
 import ReactstrapButtonstate from './ReactstrapButtonstate';
 import ReactstrapCheckedRadio from './ReactstrapCheckedRadio';
+import DropDirectionVariations from './DropDirectionVariations';
 
 export default class extends Component {
   render() {
@@ -14,8 +15,9 @@ export default class extends Component {
           <ReactstrapBasicButtons /><br/>
           <ReactstrapOutlineButtons /><br/>
           <ReactstrapButtonSizes /><br/>
+          <DropDirectionVariations /><br/><br/>
           <ReactstrapButtonstate /><br/>
-          <ReactstrapCheckedRadio />
+          <ReactstrapCheckedRadio /><br/>
       </Fragment>
      )
    }
